530f1ccce40503d5bb96eed594e9029beec9f4d7
Ramb Memburg <ramb.memburg@gmail.com>
2023-11-19 14:20:35 -0500

    re componing

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # memburg.github.io
 
-If you take a look the source code you may get a stroke, but sincerly I just didn't want to install +1400 node packages to create a static site.
+If you take a look the source code you may get a stroke, but sincerly I just didn't want to install +1400 node packages to create a static web site.
 
 Sometimes a man is just too old to build a node project...
--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,19 @@
     const body = document.querySelector('body');
 
     // Load the components
+    const navbar = await loadComponent('navbar');
     const header = await loadComponent('header');
 
     // Attach the components
-    body.insertAdjacentHTML('beforeend', header);
+    body.insertAdjacentHTML('afterbegin', header);
+    body.insertAdjacentHTML('afterbegin', navbar);
+
+    // Get main tag to be able to attach components to it
+    const main = document.querySelector('main');
+
+    // Load the components
+    const shortbio = await loadComponent('shortbio');
+
+    // Attach components to the main tag
+    main.insertAdjacentHTML('beforeend', shortbio);
 })();
