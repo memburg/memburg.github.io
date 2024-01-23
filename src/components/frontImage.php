40e822140ba5562a4dca59d1be8b6c5ab8ca89b5
Ramb Memburg <ramb.memburg@gmail.com>
2024-01-22 20:35:57 -0500

    room front image

---
+++ src/components/frontImage.php
@@ -0,0 +1,17 @@
+<style>
+    .front-image {
+        display: flex;
+        justify-content: center;
+        height: 40rem;
+
+        & img {
+            width: 30rem;
+            height: 30rem;
+            align-self: center;
+        }
+    }
+</style>
+
+<div class="front-image">
+    <img src="./media/room.png" alt="voxel art image of my old bedroom">
+</div>
