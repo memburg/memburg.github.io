6283602211ae91514933a378d6863e48b1ca44ed
Ramb Memburg <ramb.memburg@gmail.com>
2024-11-22 14:58:49 -0500

    mickey 8:D

--- src/components/frontImage.php
+++ src/components/frontImage.php
@@ -4,14 +4,42 @@
         justify-content: center;
         height: 40rem;
 
-        & img {
+        & textarea {
+            resize: none;
             width: 30rem;
             height: 30rem;
             align-self: center;
+            background-color: transparent;
+            color: white;
+            outline: none;
+            border: none;
+
+            background: linear-gradient(to right,
+                    var(--ctp-frappe-sapphire),
+                    var(--ctp-macchiato-teal),
+                    var(--ctp-latte-rosewater),
+                    var(--ctp-frappe-sky));
+
+            -webkit-background-clip: text;
+            background-clip: text;
+            -webkit-text-fill-color: transparent;
+            text-fill-color: transparent;
+            background-size: 500% auto;
+            animation: movingBackgroundText 2s linear infinite alternate;
+        }
+    }
+
+    @keyframes movingBackgroundText {
+        0% {
+            background-position: 0% 50%;
+        }
+
+        100% {
+            background-position: 100% 50%;
         }
     }
 </style>
 
 <div class="section front-image">
-    <img src="./media/room.png" alt="voxel art image of my old bedroom">
+    <textarea id="movie"></textarea>
 </div>
